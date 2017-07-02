3380c6759e968055cab28ce2626d973cf706a89c
Gulshan Jangid <gulshan98125@gmail.com>
2017-07-02 20:23:31 +0530

    On submit nodejs recieves items list along with steamid

--- steam trade bot/index.js
+++ steam trade bot/index.js
@@ -1,3 +1,10 @@
+const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+const SteamCommunity = require('steamcommunity');
+const TradeOfferManager = require('steam-tradeoffer-manager');
+const client = new SteamUser();
+const community = new SteamCommunity();
+
 var app = require('express')();
 
 var http = require('http').Server(app);
@@ -10,9 +17,8 @@ var querystring = require('querystring');
 var redis = require('redis');
 var sub = redis.createClient();
 
-app.get('/', function(req, res){
-  res.send('<h1>test place</h1>');
-});
+
+
 
 
 io.use(function(socket,accept){
@@ -35,64 +41,84 @@ io.on('connection', function (socket) {
     
     //Client is sending message through socket.io
     socket.on('send_message', function (message) {
-        console.log(message+"TROLOLNIGGA");
-        // values = querystring.stringify({
-        //     comment: message,
-        //     sessionid: socket.request.cookie['sessionid'],
-        // });
+
+       console.log("recieved message: "+ message);
+       var itemsIdWithSteamIdArray = JSON.parse("[" + message + "]"); // Items array with array[0] as steamid
+
         
+
+       //Below is create and send POST message to Django server
+
          form = {
             'comment': message,
             'sessionid': socket.request.cookie['sessionid'],
         }
 
-        console.log(socket.request.cookie['sessionid']);
-
-        // var options = {
-        //     host: 'localhost',
-        //     port: 3000,
-        //     path: '/node_api',
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/x-www-form-urlencoded',
-        //         'Content-Length': values.length
-        //     }
-        // };
-
         var options = {
-        	uri : 'http://localhost:4000/node_api',
-        	method : 'POST',
-        	form : form
+            uri : 'http://localhost:4000/node_api',
+            method : 'POST',
+            form : form
         }
         
-        //Send message to Django server
-        // var req = http.get(options, function(res){
-        //     res.setEncoding('utf8');
-            
-        //     //Print out error message
-        //     res.on('data', function(message){
-        //         if(message != 'Everything worked :)'){
-        //             console.log('Message: ' + message);
-        //         }
-        //     });
-        // });
         
-        // req.write(values);
-        // req.end();
 
-		request(options, function (error, response, body) {
-			console.log(options);
-			console.log("request sent");
+        request(options, function (error, response, body) {
+            
 
-			  if (!error && response.statusCode == 200) {
-			    console.log(body)
-			    console.log(response) // Print the google web page.
-			  }
-			  else {console.log("error");}
-			})
+              if (!error && response.statusCode == 200) {
+                console.log(body);
+              }
+              else {console.log("error");}
+            })
     });
 });
 
+
+const manager = new TradeOfferManager({
+    steam: client,
+    community: community,
+    language: 'en'
+});
+
+
+const logOnOptions = {
+    accountName: 'gulshan98127',
+    password: 'Montyhanda1',
+    twoFactorCode: SteamTotp.generateAuthCode('V03wm9pAENwd5HIv6DrX45xquk0=')
+};
+
+client.logOn(logOnOptions);
+
+client.on('loggedOn', () => {
+    console.log('Logged into Steam');
+    client.setPersona(SteamUser.Steam.EPersonaState.Online);
+    client.gamesPlayed(730);
+});
+
+client.on('webSession', (sessionid, cookies) => {
+    manager.setCookies(cookies);
+
+    community.setCookies(cookies);
+    community.startConfirmationChecker(10000, 'your_identity_secret');
+});
+
+manager.on('newOffer', (offer) => {
+                        if (offer.itemsToGive.length == 0 && offer.itemsToReceive.length > 0) {
+                        offer.accept((err, status) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Accepted offer. Status: ${status}.');
+            }
+        });
+                                }
+                        else {
+                        
+                            }
+    
+        
+    
+});
 
 
 
--- website/csgomiddleman/views.py
+++ website/csgomiddleman/views.py
@@ -18,106 +18,94 @@ from steamauth import RedirectToSteamSignIn, GetSteamID64
 # Create your views here.
 @login_required
 def home(request):
-    
     comments = Comments.objects.select_related().all()[0:100]
     return render(request, 'chat/index.html', locals())
 
 @csrf_exempt
 def node_api(request):
-    
     if request.method == "POST":
-        
         #Get User from sessionid
         session = Session.objects.get(session_key=request.POST.get('sessionid'))
 
         # print (request.session.session_key)
         user_id = session.get_decoded().get('_auth_user_id')
-        # print (userid)
+        print (user_id)
         user = User.objects.get(id=user_id)
-        # print (user)
-        
+
         #Create comment
         Comments.objects.create(user=user, text=request.POST.get('comment'))
-
         #Once comment has been created post it to the chat channel
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
         r.publish('chat', user.username + ': ' + request.POST.get('comment'))
-
-        return HttpResponse("Everything worked :)")
-    
+        return HttpResponse("recieved trade from: "+user.username+"("+user_id+"), msg = "+ request.POST.get('comment'))
     else:
         return HttpResponse("error :(")
 
-
 @login_required
 def dashboard(request):
-	return render(request, 'account/dashboard.html', {'user': request.user})
+    return render(request, 'account/dashboard.html', {'user': request.user})
 
 @login_required
 def steam_login_dashboard(request):
-	return render(request, 'account/steam_login_dashboard.html')
+    return render(request, 'account/steam_login_dashboard.html')
 
 @login_required
 def create_random_trade(request):
-	randomString = get_random_string(length=8, allowed_chars=u'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-	
-	trade.objects.create(user_giving_skins=request.user,trade_status="active", random_string=randomString, created_by=request.user)
-
-	return HttpResponseRedirect(reverse('trade_page', kwargs={'randomString':randomString}))
+    randomString = get_random_string(length=8, allowed_chars=u'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    trade.objects.create(user_giving_skins=request.user,trade_status="active", random_string=randomString, created_by=request.user)
+    return HttpResponseRedirect(reverse('trade_page', kwargs={'randomString':randomString}))
 
 @login_required
 def trade_page(request, randomString):
-	Profile_user_object = Profile.objects.get(user=request.user)
-	steam64id = Profile_user_object.steam_id
-	response = urllib2.urlopen('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7EC66869C567554434C440CFAD2BCEDB&steamids='+steam64id)
-	data = json.load(response)
-	for jo in data:
-		object = data[jo]['players']
-	for oj in object:
-		username = oj['personaname']
-		profile_image_url_large = oj['avatarfull']
-		profile_image_url_small = oj['avatar']
-		profile_image_url_medium = oj['avatarmedium']
-	
-	response2 = urllib2.urlopen('http://steamcommunity.com/profiles/'+steam64id+'/inventory/json/730/2')
-	data2 = json.loads(response2.read())
-	DictListofitems = data2['rgDescriptions']
-	itemslist = []
-	for guns in DictListofitems:
-		itemslist.append(DictListofitems[guns]['name'])
-	Dictidofitems = data2['rgInventory']
-	idList =[]
-	for itemsid in Dictidofitems:
-		idList.append(itemsid)
-	tupleList = list(zip(itemslist, idList))
-	for (items,itemsid) in tupleList:
-		print (items+"----===---"+itemsid)
-	context = {'itemslist':itemslist,
-        		   'profile_image_url_medium': profile_image_url_medium,
-        		   'profile_image_url_large': profile_image_url_large,
-        		   'profile_image_url_small': profile_image_url_small,
-        		   'randomString': randomString,
-        		   'username': username,
-        		   'itemsidlist': idList,
-        		   'tupleList': tupleList,
-        		   }
-	return render(request, 'trade/tradepage.html', context)
+    Profile_user_object = Profile.objects.get(user=request.user)
+    steam64id = Profile_user_object.steam_id
+    response = urllib2.urlopen('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7EC66869C567554434C440CFAD2BCEDB&steamids='+steam64id)
+    data = json.load(response)
+    for jo in data:
+        object = data[jo]['players']
+    for oj in object:
+        username = oj['personaname']
+        profile_image_url_large = oj['avatarfull']
+        profile_image_url_small = oj['avatar']
+        profile_image_url_medium = oj['avatarmedium']
+    response2 = urllib2.urlopen('http://steamcommunity.com/profiles/'+steam64id+'/inventory/json/730/2')
+    data2 = json.loads(response2.read())
+    DictListofitems = data2['rgDescriptions']
+    itemslist = []
+    guns_icon_list = []
+    for guns in DictListofitems:
+        itemslist.append(DictListofitems[guns]['name'])
+        guns_icon_list.append(DictListofitems[guns]['icon_url'])
+    Dictidofitems = data2['rgInventory']
+    idList =[]
+    for itemsid in Dictidofitems:
+        idList.append(itemsid)
+    tupleList = list(zip(itemslist, idList, guns_icon_list))
+    for (items,itemsid,guns) in tupleList:
+        print (items+"----===---"+itemsid+guns)
+    context = {'itemslist':itemslist,
+    'profile_image_url_medium': profile_image_url_medium,
+    'profile_image_url_large': profile_image_url_large,
+    'profile_image_url_small': profile_image_url_small,
+    'randomString': randomString,
+    'username': username,
+    'itemsidlist': idList,
+    'tupleList': tupleList,
+    'steamid': steam64id,
+    }
+    return render(request, 'trade/tradepage.html', context)
 
-
-# /login
 def Login(request):
-  return RedirectToSteamSignIn('/process')
+    return RedirectToSteamSignIn('/process')
 
 # /process
 def LoginProcess(request):
     steamid = GetSteamID64(request.GET)
-    
     if steamid == False:
         # login failed
         return HttpResponseRedirect('/login_failed')
     else:
         # login success
-        # profile_object = Profile.objects.create(phone_no="9971751949");
         Profile_user_object = Profile.objects.get(user=request.user)
         Profile_user_object.steam_id = steamid
         Profile_user_object.save()
@@ -125,16 +113,14 @@ def LoginProcess(request):
         response = urllib2.urlopen('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7EC66869C567554434C440CFAD2BCEDB&steamids='+steamid)
         data = json.load(response)
         for jo in data:
-        	object = data[jo]['players']
+            object = data[jo]['players']
         for oj in object:
-        	profile_image_url_medium = oj['avatarmedium']
-        	profile_image_url_large = oj['avatarfull']
-        	profile_image_url_small = oj['avatar']
+            profile_image_url_medium = oj['avatarmedium']
+            profile_image_url_large = oj['avatarfull']
+            profile_image_url_small = oj['avatar']
         context = {'steamid': steamid,
-        		   'profile_image_url_medium': profile_image_url_medium,
-        		   'profile_image_url_large': profile_image_url_large,
-        		   'profile_image_url_small': profile_image_url_small,
-        		   }
-        
-        
-        return render(request, 'account/dashboard.html', context)
+        'profile_image_url_medium': profile_image_url_medium,
+        'profile_image_url_large': profile_image_url_large,
+        'profile_image_url_small': profile_image_url_small,
+        }
+    return render(request, 'account/dashboard.html', context)
