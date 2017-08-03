75075b526408623700cbae67fd7365f7d5707f27
Gulshan Jangid <gulshan98125@gmail.com>
2017-08-03 15:15:51 +0530

    again fixing not defined bug in the above commit

--- steam trade bot/index.js
+++ steam trade bot/index.js
@@ -81,21 +81,38 @@ function depositSkinsUrlAndNames(itemsArray, partnerid, tradeUrl, randomString) 
         var url = item.getImageURL() + "128x128";
         skinsImageUrls += url + ";";
         skinsNames += item.market_hash_name +";";
-        stringToAdd = item.actions[0].link;
-        modifiedString = stringToAdd.replace("%owner_steamid%",partner)
-        modifiedString2 = modifiedString.replace("%assetid%",item.assetid)
+        if(typeof item.actions != 'undefined'){
+        	stringToAdd = item.actions[0].link;
+        	modifiedString = stringToAdd.replace("%owner_steamid%",partner)
+        	modifiedString2 = modifiedString.replace("%assetid%",item.assetid)
+        	inspectLinks += modifiedString2 + "&&&";
+        }
+        	else {
+        		stringToAdd = "#";
+        		inspectLinks += stringToAdd;
 
-        inspectLinks += modifiedString2 + "&&&";
+        }
+        
+
+        
 
     }
     var item = theirInv.find((item) => item.assetid ==''+itemsArray[itemsArray.length-1]);
     var url = item.getImageURL() + "128x128";
     skinsImageUrls += url;
     skinsNames = item.market_hash_name;
-    stringToAdd2 = item.actions[0].link;
-    modifiedString3 = stringToAdd2.replace("%owner_steamid%",partner)
-    modifiedString4 = modifiedString3.replace("%assetid%",item.assetid)
-    inspectLinks += modifiedString4
+    if(typeof item.actions != 'undefined'){
+    	stringToAdd2 = item.actions[0].link;
+	    modifiedString3 = stringToAdd2.replace("%owner_steamid%",partner)
+	    modifiedString4 = modifiedString3.replace("%assetid%",item.assetid)
+	    inspectLinks += modifiedString4;
+    }
+    	else{
+    		stringToAdd2 = "#";
+    		inspectLinks += stringToAdd2;
+    	}
+    
+    
 
 
 	form = {
